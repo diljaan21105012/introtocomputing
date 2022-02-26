711dabb747a432c6a7e628e427dd1d90e2e4fc6c
diljaan21105012 <97602159+diljaan21105012@users.noreply.github.com>
2022-02-26 17:45:46 +0530

    Add files via upload

---
+++ assignment4/assignment4_output.py
@@ -0,0 +1,74 @@
+ASSIGNMENT 4
+
+Question #1
+
+Move Disk 1 from P to Q
+Move Disk 2 from P to R
+Move Disk 1 from Q to R
+Move Disk 3 from P to Q
+Move Disk 1 from R to P
+Move Disk 2 from R to Q
+Move Disk 1 from P to Q
+
+Question #2
+ 
+Enter the number of rows in Pascal's Triangle: 7
+
+Using recursions:
+
+            1   
+          1   1   
+        1   2   1   
+      1   3   3   1   
+    1   4   6   4   1   
+  1   5   10   10   5   1   
+1   6   15   20   15   6   1   
+
+Using Iterative method:
+
+            1   
+          1   1   
+        1   2   1   
+      1   3   3   1   
+    1   4   6   4   1   
+  1   5   10   10   5   1   
+1   6   15   20   15   6   1   
+
+Question #3
+ 
+Enter the first integer: 8
+Enter the first integer: 6
+Remainder is:  2
+Quotient is:  1
+Both values are non zero
+{6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+{8, 5, 6, 7}
+Immutable set:  frozenset({8, 5, 6, 7})
+Largest value in the set:  8
+Hash value:  8
+
+Question #4
+ 
+Object created
+The name of the student it Diljaan and SID is 21105012.
+Object destroyed
+
+QUESTION #5
+ 
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+QUESTION #6
+ 
+Enter the first word: eat
+
+Enter a new meaningful word to test your friendship: tea
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
